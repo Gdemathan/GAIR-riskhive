6fa4c026f82cfb15bb7792e90a964f292df32c0d
Ivan Robert <ivan.rog.robert@gmail.com>
2025-01-08 21:05:45 +0100

    wip: integrate Qdrant for optimized vectorized search

--- src/client.py
+++ src/client.py
@@ -1,10 +1,17 @@
 from openai import OpenAI
 import os
+from qdrant_client import QdrantClient
 
 try:
     import dotenv
+
     dotenv.load_dotenv()
 except:
     pass
 
 openai_client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
+
+qdrant_client = QdrantClient(
+    url=os.environ.get("QDRANT_URL"),
+    api_key=os.environ.get("QDRANT_API_KEY"),
+)
